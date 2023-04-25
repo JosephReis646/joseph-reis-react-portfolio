cd48c167f1ab72663b5813da4eb6e1de9dd2afdd
joseph reis <josephreis646@gmail.com>
2023-04-24 22:35:37 -0400

    About section created

---
+++ react-portfolio/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <img src="path/to/avatar.jpg" alt="Developer" />
+      <p>Hi, my name is Joseph Reis and I am an aspiring Product manager with a niche in technology that is rooted in my experience with fullstack developemnt. In this Portfolio I hope to show you some Product and Coding projects I have worked on!</p>
+    </div>
+  );
+};
+
+export default About;
